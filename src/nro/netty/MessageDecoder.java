92e852a9ec6d0415cd90383b45ea798bd9ab7f7f
DucHuy-amateur <nightmarket6768@gmail.com>
2025-10-01 20:35:22 +0700

    update

--- src/nro/netty/MessageDecoder.java
+++ src/nro/netty/MessageDecoder.java
@@ -9,10 +9,45 @@ import nro.models.network.Message;
 public class MessageDecoder extends ByteToMessageDecoder {
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
         Session session = ctx.channel().attr(SessionManager.SESSION_KEY).get();
         if (session != null) {
-            // Logic to decode messages will be added later
+            while(in.isReadable()) {
+                byte b = in.readByte();
+                if (session.isLogin) {
+                    // Đã login thì xử lý luồng dữ liệu bình thường
+                } else {
+                    // Chưa login, xử lý handshake
+                    if (b == 20) {
+                        handshake(session, in);
+                        session.isLogin = true; // Giả định handshake thành công
+                    }
+                }
+            }
+            // Phần logic đọc message chính sẽ được chuyển vào đây
+            // Tạm thời để trống để đảm bảo cấu trúc
+        }
+    }
+
+    private void handshake(Session session, ByteBuf dis) {
+        try {
+            byte key = dis.readByte();
+            session.setCurR(key);
+            session.setCurW(key);
+
+            byte[] sendKey = new byte[key];
+            for (int i = 0; i < key; i++) {
+                sendKey[i] = dis.readByte();
+            }
+            session.setSendKey(sendKey);
+
+            byte[] recvKey = new byte[key];
+            for (int i = 0; i < key; i++) {
+                recvKey[i] = dis.readByte();
+            }
+            session.setRecvKey(recvKey);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/nro/netty/MessageEncoder.java
+++ src/nro/netty/MessageEncoder.java
@@ -8,10 +8,30 @@ import nro.models.network.Message;
 public class MessageEncoder extends MessageToByteEncoder<Message> {
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) throws Exception {
+    protected void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) {
         Session session = ctx.channel().attr(SessionManager.SESSION_KEY).get();
-        if (session != null) {
-            // Logic to encode messages will be added later
+        if (session != null && msg != null) {
+            try {
+                byte[] data = msg.getData();
+                byte command = msg.getCommand();
+                int size = data.length;
+
+                if (size <= 32767) {
+                    if (size > 255) {
+                        out.writeByte(command);
+                        out.writeShort(size);
+                    } else {
+                        out.writeByte(command);
+                        out.writeByte(size);
+                    }
+                } else {
+                    out.writeByte(command);
+                    out.writeInt(size);
+                }
+                out.writeBytes(data);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
